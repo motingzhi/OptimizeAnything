fb37ba541f1fdd1a35e6df4c92a37ab848c98685
motingzhi <57576789+motingzhi@users.noreply.github.com>
2024-03-13 19:21:58 +0200

    Update results.php

--- results.php
+++ results.php
@@ -60,13 +60,13 @@
             // Declare the x (horizontal position) scale.
             const x = d3.scaleUtc()
                                     //.domain([objectiveBounds[0],objectiveBounds[1]])
-                .domain([1,2])
+                .domain([0,2])
                 .range([marginLeft, width - marginRight]);
             
             // Declare the y (vertical position) scale.
             const y = d3.scaleLinear()
                                 //.domain([objectiveBounds[2], objectiveBounds[3]])
-                .domain([1,2])
+                .domain([0,2])
                 .range([height - marginBottom, marginTop]);
             
             // Create the SVG container.

1ab23d8bf87f943a46f9af5ae6970752683da4f2
motingzhi <57576789+motingzhi@users.noreply.github.com>
2024-03-13 20:34:54 +0200

    Update results.php

--- results.php
+++ results.php
@@ -48,8 +48,8 @@
            
         if (objectiveNames.length <= 2)
             {
-                  const width = 700;
-                  const height = 500;
+                  const width = 640;
+                  const height = 400;
                   const marginTop = 25;
                   const marginRight = 20;
                   const marginBottom = 35;

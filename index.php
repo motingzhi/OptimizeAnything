c6ae6f5803047cc37c6291c0a7f88e6640beb5ef
motingzhi <57576789+motingzhi@users.noreply.github.com>
2024-05-08 17:00:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,15 @@ if (isset($_SESSION['user_token'])) {
 <!DOCTYPE html>
 <html>
 <head>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NVHCB17JRF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NVHCB17JRF');
+</script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
         .top-bar {

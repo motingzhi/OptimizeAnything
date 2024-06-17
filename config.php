85b536309ea7413487e49b643d5921febb62e6bb
motingzhi <tingzhimo@gmail.com>
2024-06-17 21:00:41 +0300

    sql

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "12345678";
-$dbname = "optimize1";
+$username = "root"; // 替换为你的数据库用户名
+$password = "12345678"; // 替换为你的数据库密码
+$dbname = "optimize1"; // 数据库名为 optimizer
 
+// 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// 检查连接
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }

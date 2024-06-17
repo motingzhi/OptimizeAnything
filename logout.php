e3865509aa4eed096320a7b253f5bda279cd027c
motingzhi <tingzhimo@gmail.com>
2024-06-17 17:38:13 +0300

    updated upstream

--- logout.php
+++ logout.php
@@ -3,8 +3,6 @@
 session_start();
 unset($_SESSION['user_token']);
 session_destroy();
-<<<<<<< Updated upstream
 header("Location: index.php");
-=======
-header("Location: index.php");
->>>>>>> Stashed changes
+
+?>

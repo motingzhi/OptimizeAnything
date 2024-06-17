00411e7ec2c476805da10987a7dbd2e0abcbb281
motingzhi <tingzhimo@gmail.com>
2024-06-17 20:51:55 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$hostname = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "12345678";
 $dbname = "optimize1";
 
-$conn = new mysqli($hostname, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);

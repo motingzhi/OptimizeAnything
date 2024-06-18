62e63373cad3262e5294d205c3d03d87d2ddde37
motingzhi <tingzhimo@gmail.com>
2024-06-19 00:19:55 +0300

    Update index_2.php

--- index_2.php
+++ index_2.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Prolific ID is required");
     }
 
-    $stmt = $conn->prepare("INSERT INTO data (ID, Solutionlist, Savedsolutions, Savedobjectives, parameternames, parameterbounds, definetimestamp ) VALUES (?, ?, ?, ?,?,?,?)");
+    $stmt = $conn->prepare("INSERT INTO data (ID, Solutionlist, Savedsolutions, Savedobjectives, parametername, parameterbounds, definetimestamp ) VALUES (?, ?, ?, ?,?,?,?)");
     if ($stmt === false) {
         die("Prepare failed: " . $conn->error);
     }

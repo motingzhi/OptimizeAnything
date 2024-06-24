750536dcd999036f3ce75262e7d1c3fbd7f52523
motingzhi <tingzhimo@gmail.com>
2024-06-24 22:14:15 +0300

    test

--- cgi/next-evaluation.py
+++ cgi/next-evaluation.py
@@ -41,16 +41,16 @@ tester = 1
 xx = 57
 reply2 = {}
 
-# # Define the log file path
-# log_file_folder = "../python_log"
-# log_file_path = os.path.join(log_file_folder, "Next-evaluation.log")
+# Define the log file path
+log_file_folder = "../python_log"
+log_file_path = os.path.join(log_file_folder, "Next-evaluation2.log")
 
-# # Create the log file directory if it doesn't exist
-# os.makedirs(log_file_folder, exist_ok=True)
+# Create the log file directory if it doesn't exist
+os.makedirs(log_file_folder, exist_ok=True)
 
-# # Redirect stdout and stderr to the log file
-# # sys.stdout = open(log_file_path, "a")
-# sys.stderr = open(log_file_path, "a")
+# Redirect stdout and stderr to the log file
+# sys.stdout = open(log_file_path, "a")
+sys.stderr = open(log_file_path, "a")
 
 # Read provided formData
 formData = cgi.FieldStorage()

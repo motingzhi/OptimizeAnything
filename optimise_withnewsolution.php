06778db0a500b5e1daacf9e5889d6559998b8667
motingzhi <tingzhimo@gmail.com>
2024-06-24 21:34:42 +0300

    Update optimise_withnewsolution.php

--- optimise_withnewsolution.php
+++ optimise_withnewsolution.php
@@ -351,7 +351,7 @@ if (savedSolutions.length/parameterNames.length < 2*(parameterNames.length+1))
         for (var i = 0; i<parameterNames.length; i++) {
             if (savedObjectives.length == 1)
             {
-                generatedSolution[i] = parameterNames[i] + " =  " + solutionList[savedObjectives.length];
+                generatedSolution[i] = parameterNames[i] + " =  " + solutionList[savedObjectives.length+i-1];
             }
             else
             {

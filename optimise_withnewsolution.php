a46fd0db63d6d522ed43d1b9d66a9d41eeeb6fd7
motingzhi <tingzhimo@gmail.com>
2024-07-15 15:43:18 +0300

    Update optimise_withnewsolution.php

--- optimise_withnewsolution.php
+++ optimise_withnewsolution.php
@@ -317,9 +317,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 htmlNewRow += "<td contenteditable='true' class='record-data' id='display-measurement-name'> " + objectiveNames[i]  +  " </td>" 
                 htmlNewRow += "<td contenteditable='false' class='record-data' id='display-measurement-bounds'> " + "Enter measurement (" + objectiveBounds[2*i] + "-" + objectiveBounds[2*i+1]  + ")"+ " </td>" // placeholder的效果怎么做
                 htmlNewRow += "<td contenteditable='true' class='record-data' id='record-measurement' style='width: 25%;' placeholder=''> </td>"
-                // htmlNewRow += "<td id='record-data-buttons'>"
                 htmlNewRow += "</td></tr>"
-                $("#measurement-table", window.document).append(htmlNewRow)
+                $("#measurement-table", window.document).append(htmlNewRow);
             }
     }  
     

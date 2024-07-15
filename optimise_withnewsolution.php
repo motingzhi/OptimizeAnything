c6e4f808cdccb2919e3c1073a888ef6411de1f05
motingzhi <tingzhimo@gmail.com>
2024-07-15 15:38:23 +0300

    Update optimise_withnewsolution.php

--- optimise_withnewsolution.php
+++ optimise_withnewsolution.php
@@ -321,7 +321,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 htmlNewRow += "</td></tr>"
                 $("#measurement-table", window.document).append(htmlNewRow);
             }
-    }  else {
+    }  else if (savedSolutions.length/parameterNames.length > 2*(parameterNames.length+1)-1) {
 
      
 
@@ -336,7 +336,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     }
 
+    // else if (savedSolutions.length/parameterNames.length = 2*(parameterNames.length+1)-1) {
 
+var x = document.getElementById('evaluate-solution');
+    var y = document.getElementById('options')
+    var z = document.getElementById('form-options-2')
+    var z2 = document.getElementById('form-options-1')
+
+x.style.display = 'none';
+z.style.display = 'none';
+z2.style.display = 'none';
+
+}
 
 
 

c4f2a2199041df7f366df23830ea60f48b49d4ce
motingzhi <tingzhimo@gmail.com>
2024-07-15 15:44:31 +0300

    Update optimise_withnewsolution.php

--- optimise_withnewsolution.php
+++ optimise_withnewsolution.php
@@ -252,7 +252,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
         
-        var num_parameters = parameterNames.length
+        var num_parameters = parameterNames.length;
 
         try {var objectivesInput = localStorage.getItem("objectives-input").split(",");}
         catch(err) {}
@@ -287,40 +287,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (savedSolutions.length/parameterNames.length < 2*(parameterNames.length+1)-1)
     {
-        var x = document.getElementById('evaluate-solution');
-        var y = document.getElementById('options');
-        var z = document.getElementById('form-options-1');
-        var z2 = document.getElementById('form-options-2');
-        var z3 = document.getElementById('form-options-3');
 
-            if (x.style.display == 'none') {
-                x.style.display = 'block'
-                z2.style.display = 'block'
-
-                y.style.display = 'none'
-                z.style.display = 'none'
-                z3.style.display = 'none'
-
-            }
-            else {
-                x.style.display = 'none'
-                z2.style.display = 'none'
-                z3.style.display = 'none'
-
-                y.style.display = 'inline-block'
-                z.style.display = 'block'
-            }
-        
-           for (i = 0; i < objectiveNames.length; i++) {
-                var htmlNewRow = "" 
-                htmlNewRow += "<tr>" 
-                htmlNewRow += "<td contenteditable='true' class='record-data' id='display-measurement-name'> " + objectiveNames[i]  +  " </td>" 
-                htmlNewRow += "<td contenteditable='false' class='record-data' id='display-measurement-bounds'> " + "Enter measurement (" + objectiveBounds[2*i] + "-" + objectiveBounds[2*i+1]  + ")"+ " </td>" // placeholder的效果怎么做
-                htmlNewRow += "<td contenteditable='true' class='record-data' id='record-measurement' style='width: 25%;' placeholder=''> </td>"
-                htmlNewRow += "</td></tr>"
-                $("#measurement-table", window.document).append(htmlNewRow);
-            }
-    }  
+    } ; 
     
     // if (savedSolutions.length/parameterNames.length > 2*(parameterNames.length+1)-1) {
 

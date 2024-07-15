1cfaba6b65fb9f841dbc58dca25181a56d77b7ad
motingzhi <tingzhimo@gmail.com>
2024-07-15 15:35:56 +0300

    Update optimise_withnewsolution.php

--- optimise_withnewsolution.php
+++ optimise_withnewsolution.php
@@ -323,6 +323,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
     } else if(savedSolutions.length/parameterNames.length = 2*(parameterNames.length+1)-1) {
 
+        var x = document.getElementById('evaluate-solution');
+            var y = document.getElementById('options')
+            var z = document.getElementById('form-options-2')
+            var z2 = document.getElementById('form-options-1')
+
+        x.style.display = 'none';
+        z.style.display = 'none';
+        z2.style.display = 'none';
+
     } else{
 
      

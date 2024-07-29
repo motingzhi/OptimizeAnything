7851c5cc9a70e1a950d19308d89ed902635c1a32
motingzhi <tingzhimo@gmail.com>
2024-07-29 05:26:11 +0300

    Update refine-solution.py

--- cgi/refine-solution.py
+++ cgi/refine-solution.py
@@ -79,15 +79,24 @@ try:
     savedSolutions = (formData['saved-solutions'].value).split(',')
 except:
     savedSolutions = []
+# try:
+#     savedObjectives = (formData['saved-objectives'].value).split(',')
+# except:
+#     savedObjectives = []
+# try:
+#     objectivesInput = (formData['objectives-input'].value).split(',')
+# except:
+#     objectivesInput = []
+
+
 try:
     savedObjectives = (formData['saved-objectives'].value).split(',')
 except:
     savedObjectives = []
 try:
-    objectivesInput = (formData['objectives-input'].value).split(',')
+    objectivesInput = (formData['saved-objectives'].value).split(',')
 except:
     objectivesInput = []
-
 
 try:
     solutionName = (formData['solution-name'].value).split(',')
@@ -249,8 +258,8 @@ train_obj = normalise_objectives(train_obj_actual)
 
 parametersPlaceholder = []
 
-# for i in range(int(len(savedSolutions)/num_parameters)):
-#     parametersPlaceholder.append(savedSolutions[i*num_parameters:i*num_parameters+num_parameters]) #切片[2:4] 是 2，3，
+for i in range(int(len(savedSolutions)/num_parameters)):
+    parametersPlaceholder.append(savedSolutions[i*num_parameters:i*num_parameters+num_parameters]) #切片[2:4] 是 2，3，
 
 parametersPlaceholder.append(currentSolutions[-num_parameters:])
 

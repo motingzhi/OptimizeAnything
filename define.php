ae7cfe111abb83c317db5fa33de4dc303a489f10
motingzhi <tingzhimo@gmail.com>
2024-08-05 23:23:08 +0300

    Update define.php

--- define.php
+++ define.php
@@ -127,7 +127,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             height: 2px;
             background: #ddd;
             position: absolute;
-            top: 20%;
+            top: 80%;
             right: 0;
             width:100%;
             z-index: -1;

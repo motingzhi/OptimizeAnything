74eccab9e12d8a36570e9d4331347eb42e823713
motingzhi <tingzhimo@gmail.com>
2024-08-07 11:27:19 +0300

    Update define.php

--- define.php
+++ define.php
@@ -48,92 +48,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
-        .top-bar {
-            position: fixed;
-            top: calc(100vh / 12);
-            width: 100%;
-            background: transparent;
-            padding: 10px 0;
-            box-shadow: none;
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
         }
-
-        .centered-content {
-            overflow-y: auto; /* 添加垂直滚动条 */
-            max-height: calc(100vh - 350px); /* 计算中间内容的最大高度减去top-bar和bottom-bar的高度 */
-            margin-top: calc(100vh / 10 + 100px); /* Offset by the height of top-bar */
+        .container-fluid {
+            display: flex;
+            height: 100vh;
+            justify-content: space-between;
+        }
+        .card-custom {
+            width: 33.3%;
+            padding: 20px;
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: center;
+        }
+        .main-content {
+            width: 33.3%;
+            padding: 20px;
+            box-sizing: border-box;
             text-align: center;
-            width: 50%; /* Content width as 1/3 of the page */
-            margin-left: auto;
-            margin-right: auto;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
         }
-
-        .bottom-bar {
-            position: fixed;
-            /* margin-top: 100px; */
-            bottom: 0px;
-            width: 100%;
-            background: #f8f9fa; /* Light grey background similar to Bootstrap's default navbar */
-            padding: 10px 0;
-            /* box-shadow: none; */
-             /* Shadow for the bottom bar */
-            box-shadow: 0 -2px 4px rgba(0,0,0,0.1); /* Shadow for the bottom bar */
-        }
-
-        #loadingContainer {
-            display: none;
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-        }
-
-        #loadingIcon {
-            border: 8px solid #f3f3f3;
-            border-radius: 50%;
-            border-top: 8px solid #53A451;
-            width: 50px;
-            height: 50px;
-            animation: spin 1s linear infinite;
-        }
-
-        @keyframes spin {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
-        }
-
-        #loadingText {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        /* .record-data {
-            color: black;
-        } */
         .stepper {
             display: flex;
-            justify-content: space-between;
+            justify-content: center;
             margin-bottom: 20px;
-            width: 80%;
         }
-
         .step {
             flex-grow: 1;
             text-align: center;
             position: relative;
         }
-
         .step:not(:last-child)::after {
             content: '';
             height: 2px;
             background: #ddd;
             position: absolute;
-            top: 30%;
-            right: 100;
-            /* right: 0%; */
-            width:100%;
+            top: 50%;
+            right: -50%;
+            width: 100%;
             z-index: -1;
         }
-
         .step span {
             display: inline-block;
             padding: 10px 20px;
@@ -141,79 +103,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             border-radius: 50%;
             border: 2px solid #ddd;
         }
-
         .step.active span {
             font-weight: bold;
             color: #007bff;
             border-color: #007bff;
             background: white;
         }
-
-        .custom-card {
-            margin: 10px; /* 外边距 */
-            display: inline-block; /* 使卡片宽度根据内容自适应 */
-            width: 60%;
-        }
-        .custom-card .card-body {
-            padding: 10px; /* 内边距 */
-            text-align: left;
-
-        }
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            text-align: center;
-        }
-        .container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-        .description {
-            font-size: 1em;
-            margin-bottom: 20px;
-        }
-        .highlight {
-            display: inline-flex;
-            align-items: center;
-            font-size: 1em;
-        }
-        .underline {
-            margin: 0 5px;
-            padding: 5px 10px;
-            border-bottom: 2px solid;
-            font-size: 1em;
-        }
-        .variables {
-            border-bottom-color: #000000;
-            color: #E08AE9;
-        }
-        .objectives {
-            border-bottom-color: #000000;
-            color: #fb923c;
-        }
-        .normal {
-            border-bottom-color: #000000;
-            color: #000000;
-        }
-        /* .inline-input {
-            width: auto;
-            display: inline-block;
-            min-width: 100px;
-            max-width: 200px;
-        } */
-        .colored-placeholder::placeholder {
-            color: blue;
-        }
         .tooltip-container {
             position: relative;
             display: inline-block;
         }
-
         .tooltip-container .tooltip-text {
             visibility: hidden;
             width: 200px;
@@ -230,113 +129,108 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             opacity: 0;
             transition: opacity 0.3s;
         }
-
         .tooltip-container:hover .tooltip-text {
             visibility: visible;
             opacity: 1;
         }
+        .table-container {
+            max-height: 400px;
+            overflow-y: auto;
+        }
+        .separator {
+            border-left: 1px solid #ddd;
+            height: 100%;
+        }
+        .bottom-bar {
+            width: 100%;
+            background: #f8f9fa;
+            padding: 10px 0;
+            box-shadow: 0 -2px 4px rgba(0,0,0,0.1);
+        }
+        .top-bar {
+            width: 100%;
+            padding: 10px 0;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+        }
     </style>
 </head>
 <body>
-    <div class="top-bar">
-        <!-- <div class="container d-flex justify-content-between align-items-center"> -->
-        <div class="container">
-
-            <div class="stepper">
+    <div class="container-fluid">
+        <div class="card card-custom">
+            <div class="card-body">
+                <p class="text-primary">Your optimization task:</p>
+                <label>Imagine you have decided to eat more healthily. You want to choose a diet that is enjoyable, helps you lose weight, and keeps you healthy at the same time. What variables and objectives will you specify here?</label>
+            </div>
+        </div>
+        <div class="separator"></div>
+        <div class="main-content">
+            <div class="top-bar">
+                <div class="stepper">
                     <div class="step active">
-                    <span>1</span>
-                    <div>Specify Variables</div>
+                        <span>1</span>
+                        <div>Specify Variables</div>
                     </div>
                     <div class="step">
-                    <span>2</span>
-                    <div>Specify Objectives</div>
+                        <span>2</span>
+                        <div>Specify Objectives</div>
                     </div>
                     <div class="step">
-                    <span>3</span>
-                    <div>Confirm Definition</div>
+                        <span>3</span>
+                        <div>Confirm Definition</div>
                     </div>
-            </div>
-
-
-        </div>
-
-    </div>
-    
-    <div class="centered-content">
-    <!-- <form action="tutorial_1.php">
-                <button type="submit" class="btn btn-outline-primary">Tutorial</button>
-            </form>     -->
-    
-        <div class="container">
-            <div class="card custom-card">
-            <p class="text-primary"> Your optimization task:</p>
-                <div class="card-body">
-                        <label > Imagine you have decided to eat more healthily. You want to choose a diet that is enjoyable, helps you lose weight, and keeps you healthy at the same time. What variables and objectives will you specify here?</label></br>
                 </div>
             </div>
-        </div>
-
-        <div class="container">
-            <div class="card custom-card">
-            <p class="text-primary"> Your specification overview:</p>
-                <div class="card-body">
-                            You want to optimize
-                            <span class="tooltip-container">
-                                <input type="text" id="defineWhat" class="form-control mb-2 inline-input" placeholder="Variables" readonly>
-                                <span class="tooltip-text">to be specified</span>
-                            </span>                            <!-- <span class="underline">Variables: To be specified</span> -->
-                            by
-                            <span class="normal">minimizing/maximizing</span></span> 
-                            <span class="tooltip-container">
-                                    <input type="text" id="defineWhat" class="form-control mb-2 inline-input" placeholder="Objectives" readonly>
-                                    <span class="tooltip-text">to be specified</span>
-                            </span>
-                    </div>
-                        
+            <div class="table-container">
+                <label style="margin-bottom: 20px;">Variables</label>
+                <table class="table table-bordered" id="parameter-table">
+                    <thead>  
+                        <tr>  
+                            <th id="record-parameter-name" width="40%"> Variable Name </th>   
+                            <th id="record-parameter-unit" width="40%"> Unit(if have) </th>   
+                            <th id="record-parameter-lower-bound"> Minimum </th>  
+                            <th id="record-parameter-upper-bound"> Maximum </th>  
+                            <th class="delete"> Delete </th>   
+                        </tr>  
+                    </thead>  
+                    <tbody>
+                    </tbody>
+                </table>
+                <button class="btn btn-outline-primary" id="add-record-button" onclick="addDesignParametersTable()">Add Variable</button>
             </div>
-
-            <div class="card custom-card">
+            <div class="bottom-bar">
+                <button class="btn btn-primary" id="finish-objectives-button" style="width: 20%;" onclick="finishObjs()">Next</button>
+            </div>
+        </div>
+        <div class="separator"></div>
+        <div class="card card-custom">
+            <div class="card-body">
                 <p class="text-primary">Hints</p>
-
-                <div class="card-body">
-                        <label >1. The solution generated by AI will be constructed by <strong>Variables</strong> specified here, and the value generated will be inside the minimum and maximum values you specified.</label></br>
-                        <label >2. The <strong>Objectives</strong> are the criteria to evaluate the solution generated by AI.</label></br>
-                        <label >3. The specification for <strong>Variables</strong> shall not be equal to <strong>Objectives</strong>.</label></br>
+                <label>1. The solution generated by AI will be constructed by <strong>Variables</strong> specified here, and the value generated will be inside the minimum and maximum values you specified.</label><br>
+                <label>2. The <strong>Objectives</strong> are the criteria to evaluate the solution generated by AI.</label><br>
+                <label>3. The specification for <strong>Variables</strong> shall not be equal to <strong>Objectives</strong>.</label><br>
+            </div>
+            <div class="card-body">
+                <p class="text-primary">Your specification overview:</p>
+                <div>
+                    You want to optimize
+                    <span class="tooltip-container">
+                        <input type="text" id="defineWhat" class="form-control mb-2 inline-input" placeholder="Variables" readonly>
+                        <span class="tooltip-text">to be specified in the table below</span>
+                    </span>
+                    by
+                    <span class="normal">minimizing/maximizing</span>
+                    <span class="tooltip-container">
+                        <input type="text" id="defineWhat" class="form-control mb-2 inline-input" placeholder="Objectives" readonly>
+                        <span class="tooltip-text">to be specified in the table below</span>
+                    </span>
                 </div>
             </div>
         </div>
-
-
-        <label style="margin-bottom: 20px;">Variables</label></br>
-        <!-- <label style="margin-bottom: 20px;">For example, you can input gram of fruit, or portions of spagetti, or gram of carbon, etc</label> -->
-
-        <table class="table table-bordered" id="parameter-table">
-            <thead>  
-                <tr>  
-                    <th id="record-parameter-name" width="40%"> Variable Name </th>   
-                    <th id="record-parameter-unit" width="40%"> Unit(if have) </th>   
-                    <th id="record-parameter-lower-bound"> Minimum </th>  
-                    <th id="record-parameter-upper-bound"> Maximum </th>  
-                    <th class="delete"> Delete </th>   
-                </tr>  
-            </thead>  
-            <tbody>
-
-  
-            </tbody>
-        </table>
-        <button class="btn btn-outline-primary" id="add-record-button" onclick="addDesignParametersTable()">Add Variable</button>
     </div>
-    
+
     <div id="loadingContainer">
         <div id="loadingIcon"></div>
         <div id="loadingText">Loading...</div>
-    </div>
-
-    <div class="bottom-bar">
-        <div class="container text-right">
-            <button class="btn btn-primary" id="finish-objectives-button" style="width: 20%;" onclick="finishObjs()">Save</button>
-        </div>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>

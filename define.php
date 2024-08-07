ceb17ae6ea3a393a8a5941a92fa0ee2299a0950e
motingzhi <tingzhimo@gmail.com>
2024-08-07 23:43:18 +0300

    Update define.php

--- define.php
+++ define.php
@@ -349,7 +349,7 @@ if (!isset($_SESSION['ProlificID'])) {
         } catch (err) {
         // 如果发生异常，例如 "saved-objectives" 不存在，赋值一个空数组
         var parameterNames = [];
-        document.querySelector('.tooltip-container').classList.add('show-tooltip');
+        // document.querySelector('.tooltip-container').classList.add('show-tooltip');
 
         }
 

1aed681a9753ab7cc7749af263c3f448e0e15ab3
motingzhi <tingzhimo@gmail.com>
2024-08-12 16:38:48 +0300

    Update optimise_withnewsolution.php

--- optimise_withnewsolution.php
+++ optimise_withnewsolution.php
@@ -238,8 +238,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <br>
     <div class="container">
         <div id="evaluate-solution" style="display: none;">
-            <label for="solution_name">Name the alternative: </label>
+            <label for="solution_name"><strong>Name the alternative</strong> </label><br>
             <input size="40" id = "solution_name" placeholder="Give a memorable name to this idea"><br><br>
+
+            <label for="solution_name"><strong>Enter your measurements</strong> </label><br>
             <table class="table table-bordered" id="measurement-table" class="measurement-table" width="100%">
                 <tbody>
                 </tbody>

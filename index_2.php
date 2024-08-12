e6bd7d9b3cab31cea347196d07b8cb6cb90412d3
motingzhi <tingzhimo@gmail.com>
2024-08-12 18:14:43 +0300

    Update index_2.php

--- index_2.php
+++ index_2.php
@@ -174,7 +174,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
                 </div>
             </div> -->
-            <div class="col-md-4 mb-3">
+            <div class="col-md-4">
                 <a href="tutorial_1.php" class="card-link">
                     <div class="card fixed-size-card">
                         <div class="card-body">

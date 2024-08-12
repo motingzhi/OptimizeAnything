76b278e954c5572f8533a847d19e86bb92c60d6e
motingzhi <tingzhimo@gmail.com>
2024-08-12 15:05:55 +0300

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -520,10 +520,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             function populateFields() {
                 const variablesContainer = document.getElementById('variables');
                 parameterNames.forEach((variable, index) => {
-                    const button = document.createElement('button');
-                    button.className = 'btn btn-secondary variable';
-                    button.textContent = variable;
-                    variablesContainer.appendChild(button);
+                    const div = document.createElement('div');
+                    div.className = 'to-objective';  // 使用与 to-objective 相同的样式
+                    div.textContent = variable;
+                    variablesContainer.appendChild(div);
 
                     if (index < parameterNames.length - 1) {
                         const plusSign = document.createElement('div');
@@ -545,6 +545,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 // 默认选中第一个objective
                 updateSelectedObjective(selectedObjectiveIndex);
             }
+
 
             window.addEventListener('resize', drawLines);
             document.addEventListener('DOMContentLoaded', populateFields);

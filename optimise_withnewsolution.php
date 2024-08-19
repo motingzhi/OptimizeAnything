cd93e44423968fad48631c77d11d824dbf474455
motingzhi <tingzhimo@gmail.com>
2024-08-19 15:46:30 +0300

    Update optimise_withnewsolution.php

--- optimise_withnewsolution.php
+++ optimise_withnewsolution.php
@@ -276,8 +276,8 @@ if (!isset($_SESSION['ProlificID'])) {
         </div>
 
         <div id="form-options-1" style="display: inline-block; margin: 0 auto;">
-                <button class="btn btn-primary" id="next-button" onclick="nextEvaluation()">Give me the next one</button>
-                <button class="btn btn-outline-primary" id="refine-button" onclick="refineSolution()">I want to explore solution similar to this one</button>
+                <button class="btn btn-primary" id="next-button" onclick="nextEvaluation()">Save and give me the next one</button>
+                <button class="btn btn-outline-primary" id="refine-button" onclick="refineSolution()">I want to explore solutions similar to this one</button>
         </div>
 
         
